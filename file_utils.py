22271df41abdbcc53b44ddd24edb9920e6c11c1b
samini <noemail>
2018-11-24 23:21:11 -0800

    Remove special characters from game titles

--- file_utils.py
+++ file_utils.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
+
 def read_game_names(filename):
   with open(filename) as f:
     content = f.readlines()
     if content:
-      content = map(lambda x: x.rstrip('\r\n').split(',')[0].decode('utf-8','ignore').encode('utf-8'), content)
+      content = map(lambda x: x.rstrip('\r\n')
+              .split(',')[0]
+              .decode('utf-8','ignore')
+              .encode('utf-8')
+              .replace('®','')
+              .replace('™',''), content)
       return content
