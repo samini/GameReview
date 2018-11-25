e375bb88c35805e31b9528ec7a106f535e6c30c3
samini <noemail>
2018-11-24 23:35:55 -0800

    Preliminary print out of first game reviews

--- game_reviews.py
+++ game_reviews.py
@@ -2,6 +2,7 @@
 
 from file_utils import read_game_names
 from flag_parser import FlagParser
+from review_endpoint import ReviewEndpoint
 import sys
 
 def print_games(games):
@@ -22,3 +23,6 @@ if __name__ == "__main__":
   games = read_game_names(options.input_file)
   if v:
     print_games(games)
+  re = ReviewEndpoint(options.api_key, v)
+  for game in games:
+    print(re.get_review(game))
